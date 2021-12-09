428f63b134ac97ce790c53239709dca3de1b8a09
omerthecoder <okanra@uaa.k12.tr>
2021-12-09 13:22:32 +0300

    updated

--- WindowsFormsApp11/liste.cs
+++ WindowsFormsApp11/liste.cs
@@ -41,53 +41,143 @@ namespace WindowsFormsApp11
 
         public int Count { get { return _data.Length; } }
 
-        public object SyncRoot => throw new NotImplementedException();
+        public object SyncRoot { get {return _data.SyncRoot; } }
 
-        public bool IsSynchronized => throw new NotImplementedException();
+        public bool IsSynchronized { get { return _data.IsSynchronized; } }
 
         public int Add(object value)
         {
-            throw new NotImplementedException();
+            Array.Resize(ref _data, _data.Length + 1);
+            _data[_data.Length - 1] = value;
+            return 1;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            Array.Resize(ref _data, 0);
         }
 
         public bool Contains(object value)
         {
-            throw new NotImplementedException();
+            EqualityComparer<object> ec = EqualityComparer<object>.Default;
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (ec.Equals(_data[i], value)) return true;
+            }
+            return false;
         }
 
         public void CopyTo(Array array, int index)
         {
-            throw new NotImplementedException();
+            Array.Copy(_data, 0, array, index, _data.Length);
         }
 
         public IEnumerator GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new dataEnumerator(_data);
         }
 
         public int IndexOf(object value)
         {
-            throw new NotImplementedException();
+            return Array.IndexOf(_data, value, 0, _data.Length);
         }
 
         public void Insert(int index, object value)
         {
-            throw new NotImplementedException();
+            if (index >= _data.Length)
+            {
+                return;
+            }
+            Array.Resize(ref _data, _data.Length + 1);
+            object[] temp = new object[_data.Length];
+            Array.Copy(_data, temp, _data.Length);
+            for (int i = index; i < _data.Length - 1; i++)
+            {
+                _data[i + 1] = temp[i];
+            }
+            _data[index] = value;
         }
 
         public void Remove(object value)
         {
-            throw new NotImplementedException();
+            object[] tempArray = null;
+            int index = 0;
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (_data[i].Equals(value))
+                {
+                    _data[i] = default(object);
+                    index = i;
+                    tempArray = _data;
+                    break;
+                }
+            }
+            if (tempArray == null)
+            {
+                throw new ArgumentNullException();
+            }
+            for (int i = index; i < tempArray.Length; i++)
+            {
+                if (i >= _data.Length - 1)
+                {
+                    Array.Resize(ref tempArray, tempArray.Length - 1);
+                    break;
+                }
+                if (tempArray[i].Equals(default(object)))
+                {
+                    tempArray[i] = _data[i + 1];
+                    tempArray[i + 1] = default(object);
+                    continue;
+                }
+            }
+            _data = tempArray;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if(index >= _data.Length)
+            {
+                return;
+            }
+            if (index < _data.Length - 1)
+            {
+                Array.Copy(_data, index + 1, _data, index, _data.Length - 1 - index);
+            }
+            Array.Resize(ref _data, _data.Length - 1);
+        }
+        class dataEnumerator: IEnumerator
+        {
+            object[] temp;
+            int index = -1;
+            public dataEnumerator(object[] iterationArray)
+            {
+                temp = iterationArray;
+            }
+            public object Current
+            {
+                get
+                {
+                    return temp[index];
+                }
+            }
+
+            object IEnumerator.Current => throw new NotImplementedException();
+
+            public void Dispose()
+            {
+                GC.SuppressFinalize(this);
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+                return index < temp.Length;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
         }
     }
 }
